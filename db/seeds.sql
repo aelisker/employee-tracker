e8bdf2833e80ad6c537d314a72cca970c06931a1
Aaron Lisker <aelisker@gmail.com>
2020-05-27 18:53:04 -0700

    initial commit

---
+++ db/seeds.sql
@@ -0,0 +1,22 @@
+INSERT INTO department (name)
+VALUES
+  ('I.T.'),
+  ('Sales'),
+  ('Application Development'),
+  ('H.R.'),
+  ('Marketing'),
+  ('Finance and Accounting');
+
+INSERT INTO role (title, salary, department_id)
+VALUES
+  ('I.T. Helpdesk', 60000, 1),
+  ('Systems Admin', 90000, 1),
+  ('Product Specialist', 50000, 2),
+  ('Sales Lead', 65000, 2),
+  ('Frontend Developer', 100000, 3),
+  ('Backend Developer', 105000, 3),
+  ('H.R. Staff', 68000, 4),
+  ('Social Media Marketer', 70000, 5),
+  ('Traditional Marketing', 72000, 5),
+  ('In House Accountant', 85000, 6),
+  ('Financial Analyst', 80000, 6);
